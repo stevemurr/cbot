001b30ab07aa81ccccaf5a4f8815da2aacd59753
Steven Murr <stevemurr@gmail.com>
2021-11-27 19:18:07 -0800

    update asset

--- asset/asset.go
+++ asset/asset.go
@@ -7,14 +7,11 @@ import (
 )
 
 type Asset struct {
-	Name string
-	Tradeable
+	Name string `json:"name"`
 	Trendable
 }
 
-func (a *Asset) Trade(m coinbasepro.Message) {
-
-}
+func (a *Asset) Trade(m coinbasepro.Message) {}
 
 func (a *Asset) LastNMinutes(float64) Point {
 	return Point{}
@@ -28,14 +25,12 @@ func (a *Asset) LastLocalMaximum() Point {
 	return Point{}
 }
 
-type Tradeable interface {
-	Trade(coinbasepro.Message)
-}
 type Trendable interface {
 	LastNMinutes(float64) Point
 	LastLocalMinimum() Point
 	LastLocalMaximum() Point
 }
+
 type Point struct {
 	Time     time.Time
 	Average  float64
@@ -43,15 +38,4 @@ type Point struct {
 	Maximum  float64
 	STD      float64
 	Variance float64
-}
-type Action struct {
-	Asset *Asset
-	Timer *time.Timer
-}
-
-func NewAction(asset *Asset, when time.Time, fn func()) *Action {
-	return &Action{
-		Asset: asset,
-		Timer: time.AfterFunc(time.Since(when), fn),
-	}
 }
